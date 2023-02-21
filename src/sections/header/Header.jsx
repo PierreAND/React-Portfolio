64c1eba40d8bcb7776736897652334e5e175624d
PierreAND <andrieu@gmail.com>
2023-02-21 18:06:16 +0100

    wip about

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import data from './data'
 
+
 const Header = () => {
   return (
     <header id="header">
@@ -8,23 +9,23 @@ const Header = () => {
       <div className="header__profile">
         <img src="/public/zappa.jpg" alt="Header Portrait" />
       </div>
-      <h3>ANDRIEU Pierre</h3>
+      <h3>Pierre Andrieu</h3>
       <p>
         You are a click away from building your best website or web app. Im fully available for building modern, mobile responsive, highly performant website today !
       </p>
       <div className="header__cta">
-        <a href="#contact">Let's Talk</a>
-        <a href="#portfolio">My Work</a>
+        <a href="#portfolio" className='btn light'>My Work</a>
+        <a href="#contact" className='btn primary'>Let's Talk</a>
       </div>
+      
+      
       <div className="header__socials">
-        {
-          data.map((item) => (
-          <a href={item.link} key={item.id}>{item.icon}</a> 
-           
-
-          ))
-        }
-      </div>
+          {
+            data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
+          }
+        </div>
+  
+     
      </div>
 
     </header>
---
+++ src/sections/header/data.jsx
@@ -0,0 +1,15 @@
+
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {SiCodewars} from 'react-icons/si'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {AiOutlineGithub} from 'react-icons/ai'
+
+
+const data = [
+  {id: 1, link: "https://twitter.com/TnyGZz", icon : <AiOutlineTwitter/> },
+  {id: 2, link: "https://www.codewars.com/dashboard", icon : <SiCodewars/> },
+  {id: 3, link: "https://www.linkedin.com/in/pierre-andrieu-4103bb176/", icon : <AiOutlineLinkedin/> },
+  {id: 4, link: "https://github.com/PierreAND", icon : <AiOutlineGithub/> }
+]
+
+export default data
