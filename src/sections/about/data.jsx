9d9e6d8e43e959ff03a19892781f4d0fc4bd3e26
PierreAND <andrieu.ps@gmail.com>
2025-01-25 10:58:26 +0100

    test update surge

--- src/sections/about/data.jsx
+++ src/sections/about/data.jsx
@@ -14,8 +14,8 @@ const data = [
     {id: 3, icon: <MdArchitecture />, title: 'Architect IT', desc: 'Endpoint Engineer'},
     {id: 4, icon: <FaLinux />, title: 'Linux User', desc: 'script .sh process packages print'},
     {id: 4, icon: <FaGitlab />, title: 'DevOps Skills', desc: 'configuration Runner/ CI CD'},
-    {id: 4, icon: <FaDocker />, title: 'Container technologies', desc: 'Docker ochestration'},
-    {id: 4, icon: <FaAws />, title: 'Cloud operation', desc: '3 months work with AWS server team'},
+    {id: 4, icon: <FaDocker />, title: 'Container technology', desc: 'Docker ochestration'},
+    {id: 4, icon: <FaAws />, title: 'Cloud operator', desc: '3 months work with AWS server team'},
     {id: 4, icon: <TfiMicrosoftAlt />, title: 'Microsoft tools', desc: 'Vlan config deployment Active Directory'}
     ]
 
