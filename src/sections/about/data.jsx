58b7243ed86d2b65ea1133da42b5ef3ab591bde4
PierreAND <andrieu.ps@gmail.com>
2025-05-21 11:10:18 +0200

    update 21/05/25

--- src/sections/about/data.jsx
+++ src/sections/about/data.jsx
@@ -12,10 +12,10 @@ const data = [
     {id: 1, icon: <FaAward/>, title: 'Experience', desc: '1+ Year Working'},
     {id: 2, icon: <TbBooks/>, title: 'Projects', desc: '10+ Completed'},
     {id: 3, icon: <MdArchitecture />, title: 'Architect IT', desc: 'Endpoint Engineer'},
-    {id: 4, icon: <FaLinux />, title: 'Linux User', desc: 'script .sh process packages print'},
+    {id: 4, icon: <FaLinux />, title: 'Linux User', desc: 'script bash CA new comer AD'},
     {id: 4, icon: <FaGitlab />, title: 'DevOps Skills', desc: 'configuration Runner/ CI CD'},
     {id: 4, icon: <FaDocker />, title: 'Container technology', desc: 'Docker ochestration'},
-    {id: 4, icon: <FaAws />, title: 'Cloud operator', desc: '3 months work with AWS server team'},
+    {id: 4, icon: <FaAws />, title: 'Cloud operator', desc: ' Cloud Practitioner Certified'},
     {id: 4, icon: <TfiMicrosoftAlt />, title: 'Microsoft tools', desc: 'Vlan config deployment Active Directory'}
     ]
 
