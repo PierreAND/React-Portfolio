9dea4daf4322fb785ced8fb72e0525f6136cc601
PierreAND <andrieu.ps@gmail.com>
2025-11-14 09:10:32 +0100

    14/15  few edit contents pandrieu / Fri Nov 14 09:10:32 CET 2025

--- src/sections/about/data.jsx
+++ src/sections/about/data.jsx
@@ -14,9 +14,9 @@ const data = [
     {id: 3, icon: <MdArchitecture />, title: 'Architect IT', desc: 'Endpoint Engineer'},
     {id: 4, icon: <FaLinux />, title: 'Linux User', desc: 'CompTIA Linux XK-006 (V8) '},
     {id: 4, icon: <FaGitlab />, title: 'DevOps Skills', desc: 'configuration Runner/ CI CD'},
-    {id: 4, icon: <FaDocker />, title: 'Container technology', desc: 'Docker ochestration'},
+    {id: 4, icon: <FaDocker />, title: 'Container technology', desc: 'Docker / Kubernetes'},
     {id: 4, icon: <FaAws />, title: 'Cloud operator', desc: ' Cloud Practitioner Certified'},
-    {id: 4, icon: <TfiMicrosoftAlt />, title: 'Microsoft tools', desc: 'Vlan config deployment Active Directory'}
+    {id: 4, icon: <TfiMicrosoftAlt />, title: 'Microsoft tools', desc: 'Config deployment Active Directory'}
     ]
 
 
